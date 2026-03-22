b315914a1f4dc02ad7495b02a6139b4667ceda07
Kyrillos Ibrahim <kyrillos@example.com>
2026-03-22 04:26:43 +0200

    _

--- src/app/core/services/brand.service.ts
+++ src/app/core/services/brand.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable, tap, map } from 'rxjs';
+import { BehaviorSubject, Observable, shareReplay, tap, map } from 'rxjs';
 import { IBrand } from '../models/brand.model';
 import { API_CONFIG } from '../config/api.config';
 
@@ -10,23 +10,21 @@ const SERVER_URL = API_CONFIG.baseUrl;
 export class BrandService {
   private http = inject(HttpClient);
   private brandsSubject = new BehaviorSubject<IBrand[]>([]);
-  private loaded = false;
+  private allBrands$?: Observable<IBrand[]>;
 
   brands$ = this.brandsSubject.asObservable();
 
   getAll(): Observable<IBrand[]> {
-    if (this.loaded) {
-      return this.brands$;
+    if (!this.allBrands$) {
+      this.allBrands$ = this.http.get<IBrand[]>(`${SERVER_URL}/api/brands`).pipe(
+        map(brands => brands.map(b => ({
+          ...b,
+          image: b.image ? `${SERVER_URL}/uploads/${b.image}` : ''
+        }))),
+        tap(brands => this.brandsSubject.next(brands)),
+        shareReplay(1)
+      );
     }
-    return this.http.get<IBrand[]>(`${SERVER_URL}/api/brands`).pipe(
-      map(brands => brands.map(b => ({
-        ...b,
-        image: b.image ? `${SERVER_URL}/uploads/${b.image}` : ''
-      }))),
-      tap(brands => {
-        this.brandsSubject.next(brands);
-        this.loaded = true;
-      })
-    );
+    return this.allBrands$;
   }
 }
--- src/app/core/services/category.service.ts
+++ src/app/core/services/category.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable, tap, map } from 'rxjs';
+import { BehaviorSubject, Observable, shareReplay, tap, map } from 'rxjs';
 import { ICategory } from '../models/category.model';
 import { IBrand } from '../models/brand.model';
 import { API_CONFIG } from '../config/api.config';
@@ -11,38 +11,36 @@ const SERVER_URL = API_CONFIG.baseUrl;
 export class CategoryService {
   private http = inject(HttpClient);
   private categoriesSubject = new BehaviorSubject<ICategory[]>([]);
-  private loaded = false;
+  private allCategories$?: Observable<ICategory[]>;
 
   categories$ = this.categoriesSubject.asObservable();
 
   getAll(): Observable<ICategory[]> {
-    if (this.loaded) {
-      return this.categories$;
+    if (!this.allCategories$) {
+      this.allCategories$ = this.http.get<ICategory[]>(`${SERVER_URL}/api/categories/detailed`).pipe(
+        map(cats => cats.map(c => ({
+          id: c.id,
+          name: c.name,
+          slug: c.slug,
+          image: c.image ? (c.image.startsWith('http') ? c.image : SERVER_URL + c.image) : '',
+          description: c.description || '',
+          icon: c.icon || '',
+          productCount: c.productCount || 0,
+          subcategories: (c.subcategories || []).map(sub => ({
+            ...sub,
+            image: sub.image ? (sub.image.startsWith('http') ? sub.image : SERVER_URL + sub.image) : '',
+          })),
+          famousBrands: (c.famousBrands || []).map((b: IBrand) => ({
+            ...b,
+            image: b.image ? (b.image.startsWith('http') ? b.image : `${SERVER_URL}/uploads/${b.image}`) : '',
+          })),
+          filterTags: c.filterTags || [],
+        }))),
+        tap(cats => this.categoriesSubject.next(cats)),
+        shareReplay(1)
+      );
     }
-    return this.http.get<ICategory[]>(`${SERVER_URL}/api/categories/detailed`).pipe(
-      map(cats => cats.map(c => ({
-        id: c.id,
-        name: c.name,
-        slug: c.slug,
-        image: c.image ? (c.image.startsWith('http') ? c.image : SERVER_URL + c.image) : '',
-        description: c.description || '',
-        icon: c.icon || '',
-        productCount: c.productCount || 0,
-        subcategories: (c.subcategories || []).map(sub => ({
-          ...sub,
-          image: sub.image ? (sub.image.startsWith('http') ? sub.image : SERVER_URL + sub.image) : '',
-        })),
-        famousBrands: (c.famousBrands || []).map((b: IBrand) => ({
-          ...b,
-          image: b.image ? (b.image.startsWith('http') ? b.image : `${SERVER_URL}/uploads/${b.image}`) : '',
-        })),
-        filterTags: c.filterTags || [],
-      }))),
-      tap(cats => {
-        this.categoriesSubject.next(cats);
-        this.loaded = true;
-      })
-    );
+    return this.allCategories$;
   }
 
   getBySlug(slug: string): Observable<ICategory | undefined> {
