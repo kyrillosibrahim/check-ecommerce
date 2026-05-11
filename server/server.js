4e29165d14d46c2a6072035ebffde00d3da23ebf
Kyrillos Ibrahim <kyrillos@example.com>
2026-05-11 23:45:31 +0300

    fix: allow kaf-egypt and check-ecommerce vercel origins in CORS

--- server/server.js
+++ server/server.js
@@ -38,6 +38,8 @@ app.use((_req, res, next) => {
 const allowedOrigins = [
   process.env.CORS_ORIGIN,
   process.env.FRONTEND_URL,
+  'https://kaf-egypt.vercel.app',
+  'https://check-ecommerce.vercel.app',
   'http://localhost:4200',
   'http://localhost:4000',
   'http://localhost:5200',
